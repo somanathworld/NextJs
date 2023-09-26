147b1b0ee778d04c962055481dca3a64d0f16062
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2023-09-26 11:42:53 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,2 @@
 const {dbusername, dbpassword} = process.env
-export const  dbconnect = `mongodb+srv://${dbusername}:${dbpassword}@cluster0.spoizjd.mongodb.net/practicedb?retryWrites=true&w=majority`
+export const  dbconnect = `mongodb+srv://${dbusername}:${dbpassword}@cluster0.spoizjd.mongodb.net/fakestore?retryWrites=true&w=majority`
