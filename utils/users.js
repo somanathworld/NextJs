16ca2a19138a127e4c9ff3a8b9799e232c6a177d
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2023-10-14 19:30:07 +0530

    Delete utils/users.js

--- utils/users.js
+++
@@ -1,9 +0,0 @@
-export const users = [
-    { id: 101, name: "Raja1", age: 23, email: "Raja1@gamil.com", city: "Hyd1" },
-    { id: 102, name: "Raja2", age: 25, email: "Raja2@gamil.com", city: "Hyd2" },
-    { id: 103, name: "Raja3", age: 27, email: "Raja3@gamil.com", city: "Hyd3" },
-    { id: 104, name: "Raja4", age: 29, email: "Raja4@gamil.com", city: "Hyd4" },
-    { id: 105, name: "Raja5", age: 31, email: "Raja5@gamil.com", city: "Hyd5" },
-    { id: 106, name: "Raja6", age: 33, email: "Raja6@gamil.com", city: "Hyd6" },
-  ];
-  
