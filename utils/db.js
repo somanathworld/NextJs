78fd733b2e9ee476d09ddee45acfa77386578c35
Somanath Mishra <84952279+somanathworld@users.noreply.github.com>
2023-10-14 19:42:37 +0530

    Delete utils directory

--- utils/db.js
+++
@@ -1,2 +0,0 @@
-const {dbusername, dbpassword} = process.env
-export const  dbconnect = `mongodb+srv://${dbusername}:${dbpassword}@cluster0.spoizjd.mongodb.net/fakestore?retryWrites=true&w=majority`
